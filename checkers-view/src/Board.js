3eb674326e799e19290be21b1f2911bb05a59e23
АтчановА <@DESKTOP-GG3G2B2>
2019-10-08 21:35:08 +0600

    change board logic

--- checkers-view/src/Board.js
+++ checkers-view/src/Board.js
@@ -7,121 +7,161 @@ class Board extends React.Component {
         super(props)
         this.state = {
             board:props.board,
-            fieldTake:null
+            fieldTake:null,
+            checkDelete:[]
         }
-        this.FiledClick = this.FiledClick.bind(this);
-        this.FieldsLight = this.FieldsLight.bind(this);
-        this.FiledLook = this.FiledLook.bind(this);
-        this.FieldsWithCheck = this.FieldsWithCheck.bind(this);
+      this.FiledClick = this.FiledClick.bind(this);
+      this.FiledLook = this.FiledLook.bind(this);
     }
     
     FiledClick(e,item){
+        var field = e.props.field;
+        var checkDelete = this.state.checkDelete;
         var fields = this.state.board.Fields;
+        if(field.FieldTypeId!==3){
         fields.filter(i=>i.FieldTypeId==3).forEach(element => {
             element.FieldTypeId=2;
         });         
+         }
 
-        var field = e.props.field;
-        this.setState({fieldTake:field});
+
         if(field.Check){
+            this.setState({fieldTake:field});
             this.FiledLook(field,field);
+        }else{
+            if(field.FieldTypeId==3){
+                console.log(checkDelete);
+                checkDelete.forEach(i=>i.Check=null);
+                field.Check=this.state.fieldTake.Check;
+            }
+            this.setState({fieldTake:null});
+            checkDelete=[];
+            fields.filter(i=>i.FieldTypeId==3).forEach(element => {
+                element.FieldTypeId=2;
+            });         
         }
     }
 
-    FieldsLight(field,fieldTake){
-                field.FieldTypeId = 3;        
+    FiledLook(field,take){
+        var fields = this.state.board.Fields;
+        var checkDelete = this.state.checkDelete;
+        var fieldLU = fields.find(i=>i.PozX==field.PozX-1 && i.PozY==field.PozY-1);
+        var fieldLD = fields.find(i=>i.PozX==field.PozX-1 && i.PozY==field.PozY+1);
+        var fieldRU = fields.find(i=>i.PozX==field.PozX+1 && i.PozY==field.PozY-1);
+        var fieldRD = fields.find(i=>i.PozX==field.PozX+1 && i.PozY==field.PozY+1);
+        if(fieldLU){
+            if(fieldLU.Check && fieldLU.Check.CheckTypeId!=take.Check.CheckTypeId){   
+                let fieldL = fields.find(i=>i.PozX==fieldLU.PozX-1 && i.PozY==fieldLU.PozY-1); 
+                if(fieldL && !fieldL.Check && fieldL.PozX+2==take.PozX && fieldL.PozY+2==take.PozY)
+                this.DiagLeftUp(fieldL.PozX,fieldL.PozY,take);
+                let fieldR = fields.find(i=>i.PozX==fieldLU.PozX+1 && i.PozY==fieldLU.PozY-1); 
+                if(fieldR && !fieldR.Check && fieldR.PozX+2==take.PozX && fieldR.PozY+2==take.PozY)
+                this.DiagRightUp(fieldR.PozX,fieldR.PozY,take);
+                checkDelete.push(fieldLU);
+            }
+        }
+
+        if(fieldLD){
+            if(fieldLD.Check && fieldLD.Check.CheckTypeId!=take.Check.CheckTypeId){
+                let fieldL = fields.find(i=>i.PozX==fieldLD.PozX-1 && i.PozY==fieldLD.PozY+1);
+                if(fieldL && !fieldL.Check && fieldL.PozX+2==take.PozX && fieldL.PozY-2==take.PozY)
+                this.DiagLeftDown(fieldL.PozX,fieldL.PozY,take);
+                let fieldR = fields.find(i=>i.PozX==fieldLD.PozX+1 && i.PozY==fieldLD.PozY+1); 
+                if(fieldR && !fieldR.Check && fieldR.PozX+2==take.PozX && fieldR.PozY-2==take.PozY)
+                this.DiagRightDown(fieldR.PozX,fieldR.PozY,take);
+                checkDelete.push(fieldLD);
+            }
+        }
+
+        if(fieldRU){
+            if(fieldRU.Check && fieldRU.Check.CheckTypeId!=take.Check.CheckTypeId){
+                let fieldL = fields.find(i=>i.PozX==fieldRU.PozX-1 && i.PozY==fieldRU.PozY-1); 
+                if(fieldL && !fieldL.Check && fieldL.PozX-2==take.PozX && fieldL.PozY+2==take.PozY)
+                this.DiagLeftUp(fieldL.PozX,fieldL.PozY,take);
+                let fieldR = fields.find(i=>i.PozX==fieldRU.PozX+1 && i.PozY==fieldRU.PozY-1); 
+                if(fieldR && !fieldR.Check && fieldR.PozX-2==take.PozX && fieldR.PozY+2==take.PozY)
+                this.DiagRightUp(fieldR.PozX,fieldR.PozY,take);
+                checkDelete.push(fieldRU);
+            }
+        }
+
+        if(fieldRD){
+            if(fieldRD.Check && fieldRD.Check.CheckTypeId!=take.Check.CheckTypeId){
+                let fieldL = fields.find(i=>i.PozX==fieldRD.PozX-1 && i.PozY==fieldRD.PozY+1);
+                if(fieldL && !fieldL.Check && fieldL.PozX-2==take.PozX && fieldL.PozY-2==take.PozY)
+                this.DiagLeftDown(fieldL.PozX,fieldL.PozY,take);
+                let fieldR = fields.find(i=>i.PozX==fieldRD.PozX+1 && i.PozY==fieldRD.PozY+1); 
+                if(fieldR && !fieldR.Check && fieldR.PozX-2==take.PozX && fieldR.PozY-2==take.PozY)
+                this.DiagRightDown(fieldR.PozX,fieldR.PozY,take);
+                checkDelete.push(fieldRD);
+            }
+        }
+    }
+
+    SearchEmptyField(diag,take){
+        var checkDelete = this.state.checkDelete;
+        for(let i=0;i<diag.length;i+=2){
+            if(diag[i].Check || (diag.length>i+1 && !diag[i+1].Check))
+                return;
+            // if(diag.length>i+1 && diag[i+1].Check && diag[i+1].Check.CheckTypeId==take.Check.CheckTypeId){
+            //     return;
+            // }
+            
+            if(diag[i].FieldTypeId!=3){
+            diag[i].FieldTypeId=3;
+            if(i>0)
+            checkDelete.push(diag[i-1]);
+            this.DiagLeftUp(diag[i].PozX,diag[i].PozY,take);
+            this.DiagLeftDown(diag[i].PozX,diag[i].PozY,take);
+            this.DiagRightUp(diag[i].PozX,diag[i].PozY,take);
+            this.DiagRightDown(diag[i].PozX,diag[i].PozY,take);
+            }
+            
+        }
     }
     
-    FiledLook(field,fieldTake){
+    DiagLeftUp(x,y,take){
+        var diag = [];
         var fields = this.state.board.Fields;
-        if(field.PozY+1===fieldTake.PozY && field.PozX-1==fieldTake.PozX){
-            var fieldR = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY-1);
-        }else if(field.PozY+1===fieldTake.PozY && field.PozX+1==fieldTake.PozX){
-            var fieldL = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY-1);
-        }else{
-            var fieldR = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY-1);
-            var fieldL = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY-1);
+        while(x!=-1 && y!=-1){
+            diag.push(fields.find(i=>i.PozX==x && i.PozY==y));
+            x--;
+            y--;
         }
-
-        if(fieldR){
-            if(fieldR.Check && fieldR.Check.CheckTypeId!=fieldTake.Check.CheckTypeId){
-                this.FieldsWithCheck(fieldR,fieldTake);
-            }else if(!fieldL || !fieldL.Check || (fieldL.Check.CheckTypeId!=fieldTake.Check.CheckTypeId && field!=fieldTake)){
-                this.FieldsLight(fieldR,fieldTake);
-                this.FieldsWithNotCheck(fieldR,fieldTake);
-            }
-        }
-        if(fieldL){
-            if(fieldL.Check){
-                this.FieldsWithCheck(fieldL,fieldTake);
-            }else if(!fieldR || !fieldR.Check || (fieldR.Check.CheckTypeId!=fieldTake.Check.CheckTypeId && field!=fieldTake)){
-                this.FieldsLight(fieldL,fieldTake);
-                this.FieldsWithNotCheck(fieldL,fieldTake);
-            }
-        }
+        this.SearchEmptyField(diag,take);
     }
 
-    FiledLookDown(field,fieldTake){
+    DiagLeftDown(x,y,take){
+        var diag = [];
         var fields = this.state.board.Fields;
-        if(field.PozY-1===fieldTake.PozY && field.PozX-1==fieldTake.PozX){
-            var fieldR = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY+1);
-        }else if(field.PozY-1===fieldTake.PozY && field.PozX+1==fieldTake.PozX){
-            var fieldL = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY+1);
-        }else{
-            var fieldR = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY+1);
-            var fieldL = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY+1);
+        while(x!=-1 && y!=8){
+            diag.push(fields.find(i=>i.PozX==x && i.PozY==y));
+            x--;
+            y++;
         }
-      
-
-        if(fieldR){
-            if(fieldR.Check && fieldR.Check.CheckTypeId!=fieldTake.Check.CheckTypeId){
-             //   this.FieldsWithCheckDown(fieldR,fieldTake);
-            }else if(fieldL || !fieldL.Check || (fieldL.Check.CheckTypeId!=fieldTake.Check.CheckTypeId && field!=fieldTake)){
-                this.FieldsLight(fieldR,fieldTake);
-              //  this.FieldsWithNotCheck(fieldR,fieldTake);
-                
-            }
-        }
-        if(fieldL){
-            if(fieldL.Check){
-              //  this.FieldsWithCheckDown(fieldL,fieldTake);
-            }else if(!fieldR || !fieldR.Check || (fieldR.Check.CheckTypeId!=fieldTake.Check.CheckTypeId && field!=fieldTake)){
-                this.FieldsLight(fieldL,fieldTake);
-              //  this.FieldsWithNotCheck(fieldL,fieldTake);
-            }
-        }
+        this.SearchEmptyField(diag,take);
     }
 
-    FieldsWithCheckDown(field,fieldTake){
-        if(fieldTake.Check.CheckTypeId!==field.Check.CheckTypeId){
-            this.FiledLookDown(field,fieldTake);
-        }
-    }
-
-    FieldsWithCheck(field,fieldTake){
-        if(fieldTake.Check.CheckTypeId!==field.Check.CheckTypeId){
-            this.FiledLook(field,fieldTake);
-        }
-    }
-
-    FieldsWithNotCheck(field,fieldTake){
+    DiagRightUp(x,y,take){
+        var diag = [];
         var fields = this.state.board.Fields;
-        var fieldL = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY-1);
-        var fieldR = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY-1);
-        var fieldLd = fields.find(i=>i.PozX===field.PozX-1 && i.PozY===field.PozY+1);
-        var fieldRd = fields.find(i=>i.PozX===field.PozX+1 && i.PozY===field.PozY+1);
-        if(fieldR && fieldR.Check){
-        this.FieldsWithCheck(fieldR,fieldTake);
+        while(x!=8 && y!=-1){
+            diag.push(fields.find(i=>i.PozX==x && i.PozY==y));
+            x++;
+            y--;
         }
-        if(fieldL && fieldL.Check){
-        this.FieldsWithCheck(fieldL,fieldTake);
+        this.SearchEmptyField(diag,take);
+    }
+
+    DiagRightDown(x,y,take){
+        var diag = []; 
+        var fields = this.state.board.Fields;
+        while(x!=8 && y!=8){
+            diag.push(fields.find(i=>i.PozX==x && i.PozY==y));
+            x++;
+            y++;
         }
-        if(fieldRd && fieldRd.Check){
-            this.FiledLookDown(fieldRd,fieldTake);
-        }
-        if(fieldLd && fieldLd.Check){
-            this.FiledLookDown(fieldLd,fieldTake);
-        }
+        this.SearchEmptyField(diag,take);
     }
 
     render() 
